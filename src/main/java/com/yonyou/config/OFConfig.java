a5af6e107c94046ca3aec0eb559f9cc370db13bf
changjr <changjr@LAPTOP-AHFAVNGB>
2019-04-15 16:26:21 +0800

    mark

--- src/main/java/com/yonyou/config/OFConfig.java
+++ src/main/java/com/yonyou/config/OFConfig.java
@@ -1,9 +1,4 @@
-/**
- * Copyright (c) 2013-2016, Jieven. All rights reserved.
- *
- * Licensed under the GPL license: http://www.gnu.org/licenses/gpl.txt
- * To use it on other terms please contact us at 1623736450@qq.com
- */
+
 package com.yonyou.config;
 
 import java.util.HashMap;
