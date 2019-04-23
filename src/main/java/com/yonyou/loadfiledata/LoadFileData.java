6ea34e3f31bc406100cc5b831b4940845e252afe
changming <changming88@163.com>
2019-04-23 14:37:57 +0800

    增加responsebody参数

--- src/main/java/com/yonyou/loadfiledata/LoadFileData.java
+++ src/main/java/com/yonyou/loadfiledata/LoadFileData.java
@@ -3,7 +3,10 @@ package com.yonyou.loadfiledata;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -11,7 +14,6 @@ import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.jfinal.plugin.activerecord.Db;
 
 /**
  * 通过数据文件load数据到Mysql
@@ -85,4 +87,48 @@ public class LoadFileData {
 		statement.close();
 		return result;
 	}
+	
+	public static boolean exportTable2File( String tableName, String fileType ) {
+		
+		boolean flag = false;
+		
+		return flag;
+	}
+	/**
+	 * 执行SQL脚本文件
+	 * @param ip MySQL数据库所在服务器地址IP
+	 * @param host 数据库端口号
+	 * @param userName 进入数据库所需要的用户名
+	 * @param password 进入数据库所需要的密码
+	 * @param databaseName 要导入的数据库名
+	 * @param filePath SQL脚本文件目录
+	 * @param fileName SQL脚本文件名称
+	 * @throws IOException 
+	 */
+	public static void runSqlFile(String ip, String host, String userName, String password, String databaseName, String filePath, String fileName) throws IOException{
+		String cmdarray[] = { "mysql -h" + ip + " -P" + host + " -u" + userName + " -p" + password + " " + databaseName, "source " + filePath + fileName };
+		Runtime runtime = Runtime.getRuntime();
+		OutputStream os = null;
+		OutputStreamWriter writer = null;
+		try {
+			Process process = runtime.exec("cmd /c " + cmdarray[0]);//cmd之后执行数组的第一个条件进入数据库
+			//执行了第一条命令以后已经登录到mysql了
+			os = process.getOutputStream();
+			writer = new OutputStreamWriter(os);
+			writer.write(cmdarray[1]);//向图形界面输出第二第三条命令。中间 \r\n  作用是用来换行的，
+			writer.flush();
+		} finally {
+			try {
+				if (writer != null) {
+					writer.close();
+				}
+				if (os != null) {
+					os.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
