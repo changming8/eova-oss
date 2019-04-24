1cb48756cebae604092944e63d2b64b0a9256e4e
changjr <changjr@LAPTOP-AHFAVNGB>
2019-04-24 20:45:48 +0800

    表状态设置

--- src/main/webapp/ui/js/diy/test.js
+++ src/main/webapp/ui/js/diy/test.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-	alert('我是从 /eova/ui/js/test.js,产生的弹窗事件，仅用于测试菜单的自定义JS依赖');
-	
-	
+$(document).ready(function() {
+	// debugger;
+	// this;
+	// var para = window.location.search.split("=")[1];
+	// $("input[name='flow_id']").val("dsfsd");
+	// $('#flow_id').val("dsfsd");
+	// console.log($('#flow_id').val());
 });
+window.onload = function() {
+	var para = window.location.search.split("=")[1];
+	if ("undefined" == typeof para)
+		return;
+	$("input[name='flow_id']").val(para);
+	$("input[name='flow_id']").mask()
+	// $('#flow_id').val("para");
+
+}
