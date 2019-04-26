9401db2d27846197c8604357b5ed672c88fa74d7
Liuzemin <2685504598@qq.com>
2019-04-26 10:25:24 +0800

    load流，自由sql复制功能

---
+++ src/main/java/com/yonyou/controller/LoadFlowController.java
@@ -0,0 +1,44 @@
+package com.yonyou.controller;
+
+import java.util.List;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.eova.common.Easy;
+import com.eova.common.base.BaseController;
+import com.eova.common.utils.xx;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.yonyou.loadfiledata.LoadDataService;
+import com.yonyou.util.UUID;
+
+public class LoadFlowController extends BaseController {
+
+	public void copy(){
+		Object j = keepPara("rows").getAttr("rows");
+		JSONArray jsonlist = JSONArray.parseArray(j.toString());
+		JSONObject json = (JSONObject) jsonlist.get(0);
+		// 先查询 先复制以时间戳为结尾复制到FTP注册表中
+	    String metadataSql = "select * from bs_load_flow where id ='" + json.getString("id") + "'";
+	    List<Record> metadataList = Db.use(xx.DS_EOVA).find(metadataSql);
+	    String id = UUID.getUnqionPk();
+	    Record record = new Record();
+		record = metadataList.get(0).remove("ID");
+		record.set("ID", id);
+		String code = record.get("loadflow_code").toString();
+		record.remove("loadflow_code");
+		String serialCode = code + "_" + System.currentTimeMillis();
+		record.set("loadflow_code",serialCode);
+		// 插入
+    	Db.use(xx.DS_EOVA).save("bs_load_flow", record);
+    	renderJson(Easy.sucess());
+	}
+	public void test() {
+		LoadDataService lds=new LoadDataService();
+		String	id=getSelectValue("id");
+		System.out.println(id);
+		lds.loadData(id);
+		renderJson(Easy.sucess());
+		
+	}
+}
---
+++ src/main/java/com/yonyou/controller/SqlFlowController.java
@@ -0,0 +1,46 @@
+package com.yonyou.controller;
+
+import java.util.List;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.eova.common.Easy;
+import com.eova.common.base.BaseController;
+import com.eova.common.utils.xx;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.yonyou.loadfiledata.LoadDataService;
+import com.yonyou.sqlflow.SqlFlowService;
+import com.yonyou.util.UUID;
+
+public class SqlFlowController extends BaseController {
+
+	public void copy(){
+		Object j = keepPara("rows").getAttr("rows");
+		JSONArray jsonlist = JSONArray.parseArray(j.toString());
+		JSONObject json = (JSONObject) jsonlist.get(0);
+		// 先查询 先复制以时间戳为结尾复制到FTP注册表中
+	    String metadataSql = "select * from bs_sql_flow where id ='" + json.getString("id") + "'";
+	    List<Record> metadataList = Db.use(xx.DS_EOVA).find(metadataSql);
+	    String id = UUID.getUnqionPk();
+	    Record record = new Record();
+		record = metadataList.get(0).remove("ID");
+		record.set("ID", id);
+		String code = record.get("sql_code").toString();
+		record.remove("sql_code");
+		String serialCode = code + "_" + System.currentTimeMillis();
+		record.set("sql_code",serialCode);
+		// 插入
+    	Db.use(xx.DS_EOVA).save("bs_sql_flow", record);
+    	renderJson(Easy.sucess());
+	}
+	
+	public void test() {
+		SqlFlowService lds=new SqlFlowService();
+		String	id=getSelectValue("id");
+		System.out.println(id);
+		lds.sqlExcute(id);
+		renderJson(Easy.sucess());
+		
+	}
+}
